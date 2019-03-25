53fb53a4ae07b2f58709d859e149c3a108820b1f
mikkoh <me@mikkoh.com>
2019-03-25 10:54:19 -0400

    Added a getter only fixture

--- src/template/parse-property.test.ts
+++ src/template/parse-property.test.ts
@@ -5,6 +5,7 @@ import {
 
 describe('template/parseProperty', () => {
   const fixtureGetAndSet = '```\n(get) url: StringValue\n(set) url: StringSignal\n```\n\nSpecifies the URL of the texture to be downloaded.';
+  const fixtureOnlyGet = '```\n(get) state: StringSignal\n(set) (Not Available)\n```\n\nSpecifies a `StringSignal` representing the loading state of the external texture. The value of the signal is guaranteed to be a member of the `TexturesModule.ExternalTexture.State` enumeration.';
 
   test('can parse out a property', () => {
     const result = parseProperty(fixtureGetAndSet);
@@ -12,6 +13,14 @@ describe('template/parseProperty', () => {
     expect(rmWS(result)).toMatch(rmWS(`
       get url(): StringValue;
       set url(value: StringSignal);
+    `));
+  });
+
+  test('can parse out property with only get', () => {
+    const result = parseProperty(fixtureOnlyGet);
+
+    expect(rmWS(result)).toMatch(rmWS(`
+      get state(): StringSignal;
     `));
   });
 });
